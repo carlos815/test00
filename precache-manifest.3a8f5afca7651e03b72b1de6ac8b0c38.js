dbe1a01d2de4d278a3c04b622953fbb429210da2
Carlos Hernández <carlos_t815@outlook.com>
2020-07-01 15:29:39 -0400

    Updates

---
+++ precache-manifest.3a8f5afca7651e03b72b1de6ac8b0c38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e67303b35698a7152c7b09c218b9f0",
+    "url": "/test00/index.html"
+  },
+  {
+    "revision": "fd0cc95b3d7ffdfcdb11",
+    "url": "/test00/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "f3a8912e9639c9ea3ccc",
+    "url": "/test00/static/js/2.1bbe048d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test00/static/js/2.1bbe048d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd0cc95b3d7ffdfcdb11",
+    "url": "/test00/static/js/main.289c2354.chunk.js"
+  },
+  {
+    "revision": "17e226055e4799ea257a",
+    "url": "/test00/static/js/runtime-main.9ee606d9.js"
+  }
+]);
